c516427cd91991c8d66ca2224c9d3b151922ed4f
HarishRam24 <ram.harishv23@gmail.com>
2024-04-05 09:22:02 +0530

    First React Application

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <Greet />
+    </>
+  );
+}
+
+function Greet() {
+  return (
+    <div>
+      <h1>Welcome To React</h1>
+    </div>
+  );
+}
+
+export default App
